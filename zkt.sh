5b73aad696edd60d4e460d13f065ff61e34fd803
onezos <gdd9399@gmail.com>
2021-07-09 11:33:38 +0800

    更改docker安装脚本

--- zkt.sh
+++ zkt.sh
@@ -2,20 +2,21 @@
 
 function print_tips
 {
-    echo "********************************************"
+    echo '========================================================='
     echo "(1) 安装docker和docker-compose"
     echo "(2) 安装单节点zktube"
     echo "(3) 复制单节点zktube，多开节点"
-    echo "(4) 重启zktube节点"
+    echo "(4) 重启zktube节点（官方升级后，重启即可升级为最新版）"
     echo "(5) 停止并删除zktube节点"
+    echo "(6) 停止并删除所有docer container volume image"
     echo "(Q/q) 退出"
-    echo "********************************************"
+    echo '========================================================='
 }
 
 #安装docker
 function docker_install
 {
-    wget https://download.swarmeth.org/swarm/centos/docker.sh && chmod a+x docker.sh && ./docker.sh
+    wget http://download.swarmeth.org/docker/docker_install.sh && chmod a+x docker_install.sh && ./docker_install.sh
 }
 
 #新建zkt存储文件夹
@@ -24,9 +25,12 @@ function zkt_init
     rm -rf /zktdata
     cd / && mkdir zktdata && mkdir zktdata/zkt1
     cd /zktdata/zkt1
+    echo '========================================================='
     echo "请输入您的ETH钱包地址"
     read eth
     echo ${eth} >>/zktdata/zkt1/.revenue_address
+    echo "您的钱包地址是：${eth}"
+    echo '========================================================='
 }
 
 
@@ -54,22 +58,30 @@ function zkt_loop
         cp -r /zktdata/zkt1 /zktdata/zkt${i}
         cd /zktdata/zkt${i}
         docker-compose up -d
-        echo 'zkt已启动'
-    else echo 'zkt已经存在'
+        echo '========================================================='
+        echo '查看进程：docker ps'
+        echo "存放目录：/zktdata/zkt${i} "
+        echo "查看日志：docker logs -f zkt${i} _zktube-prover_1"
+        echo '============================================== Aven7 ====' 
+    else 
+        echo '========================================================='
+        echo "zkt${i} 已经存在, 跳过"
+        echo '========================================================='
     fi
     done
 }
 
 function zkt_rm
 {
-    echo "先确认/zktdata下的zkt节点数量，然后请输入zkt节点数"
-    read tCnt
+    tCnt=`ls -l|grep "zkt"|wc -l`
     for ((i=1; i<=tCnt; i ++))    
     do
     if [ ! -f /zktdata/zkt${i} ]; then
         cd /zktdata/zkt${i}
         docker-compose down
-        echo '此zkt节点已删除'
+        echo '========================================================='
+        echo "此zkt${i}节点已删除"
+        echo '========================================================='
     fi
     done
     rm -rf /zktdata
@@ -78,17 +90,32 @@ function zkt_rm
 
 function zkt_restart
 {
-    echo "先确认/zktdata下的zkt节点数量，然后请输入zkt节点数"
-    read tCnt
+    tCnt=`ls -l|grep "zkt"|wc -l`
     for ((i=1; i<=tCnt; i ++))    
     do
     if [ ! -f /zktdata/zkt${i} ]; then
         cd /zktdata/zkt${i}
         docker-compose down
         docker-compose up -d
-        echo '此zkt节点已重启'
+        echo '========================================================='
+        echo "zkt${i}节点已重启"
+        echo '========================================================='
     fi
     done
+}
+
+function zkt_docker_stop
+{
+    echo '========================================================='
+    echo '停止所有docker container'
+    docker stop $(docker container ls -aq)
+    echo '删除所有docker container'
+    docker rm $(docker container ls -aq)
+    echo '删除所有docker volume'
+    docker volume rm $(docker volume ls -aq)
+    echo '删除所有docker image'
+    docker rmi $(docker image ls -aq)
+    echo '============================================== Aven7 ====' 
 }
 
 while true
